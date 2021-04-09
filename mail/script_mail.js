a46f5f2dd3f3fcc68c32121fb2310c22290ca2a9
andreicismas <andrei_cismas@yahoo.it>
2021-04-09 18:23:48 +0200

    some coments

--- mail/script_mail.js
+++ mail/script_mail.js
@@ -1,19 +1,19 @@
+// lista array mail's
+
 var mailList = ["nmurazik@hotmail.com" ,"deckow.alexie@bins.biz","alisha65@hammes.org","konopelski.layla@pouros.org","doyle.flavie@yahoo.com","dominic.kunde@adams.net","evans.walker@gmail.com","homenick.flo@hotmail.com","telly.stroman@smith.org","flatley.curt@robel.com" ];
 console.log(mailList)
 
+// add  outher item to arrray
 mailList.push("andrei_cismas@yahoo.it")
-
 console.log(mailList)
 
+// add client mail-
 var yourMail =prompt("inserisci la tua mail " );
-// console.log("questa e la mail del cliente " +" "+  yourMail)
 
-// if (yourMail === "" || yourMail < 1 || yourMail > mailList.length){
-//     alert("inserisci una mail valore valida ")
-// }
-
+//  boolean valeu  false/true
 var mailPresente = false
 
+// cycle for
 for (var i = 0; i < mailList.length; i++){
     var mailListLog = mailList[i]
 
@@ -23,6 +23,8 @@ for (var i = 0; i < mailList.length; i++){
         document.getElementById("my_text").innerHTML=("Bravo hai caricato la mail giusta")
     }
 }
+
+// alert mail error
 
 if(!mailPresente){
     alert("mail non valida -prova- (andrei_cismas@yahoo.it)")
