ab5552869b78dc2763d3e6d0777e2d031201fe0a
andreicismas <andrei_cismas@yahoo.it>
2021-04-10 19:28:18 +0200

    some style css /end  dadi

--- dadi/script_dadi.js
+++ dadi/script_dadi.js
@@ -1,17 +1,21 @@
 
-
+// array numeri per pc e umano random 
 var generaNumeri = [numeroPc =Math.floor(Math.random() * 6) + 1 ,numeroUmanoide =Math.floor(Math.random() * 6)+ 1 ]
-console.log("nr pc"+" " +numeroPc)
-console.log("nr umano"+" "+ numeroUmanoide)
+// valori in console
+console.log("nr pc"+" " +numeroPc + " " +"'pc vince'"+" "+(numeroPc > numeroUmanoide ))
 
+console.log("nr umano"+" "+ numeroUmanoide +" " + "'umano vince'"+" "+(numeroPc < numeroUmanoide +" "+(numeroPc > numeroUmanoide )))
 
+console.log(" 'parita' "+(numeroPc === numeroUmanoide))
+
+// i valori output html
 
     if(numeroPc < numeroUmanoide ){
-        document.getElementById("my_text").innerHTML=("umano vince con" +" "+ numeroUmanoide+ " "+"nr pc " + numeroPc )
+        document.getElementById("my_text").innerHTML=("<em>umano vince con</em>" +" "+ numeroUmanoide+ " "+"nr pc " + numeroPc )
     } else if(numeroPc > numeroUmanoide){
-        document.getElementById("my_text").innerHTML=("umano perde con" +" "+ numeroUmanoide+ " "+"nr pc vince" + numeroPc )
+        document.getElementById("my_text").innerHTML=("umano perde con" +" "+ numeroUmanoide+ " "+"<em>nr pc vince</em>" +" "+ numeroPc )
     }else if(numeroPc === numeroUmanoide){
-        document.getElementById("my_text").innerHTML=("umano" +" "+ numeroUmanoide+ " "+"parita"+" "+"nr pc " + numeroPc )
+        document.getElementById("my_text").innerHTML=("umano" +" "+ numeroUmanoide+ " "+"<em>partita pari</em>"+"  "+"nr pc " + numeroPc )
     }
     
    
